3ea3ce2c2947e673719c844470abbbe7e3856388
Daniel Bui <danielbui78@gmail.com>
2017-11-02 11:27:41 -0400

    xcode changes

--- CI/no_brew.xcode.osx_10.10.sh
+++ CI/no_brew.xcode.osx_10.10.sh
@@ -4,7 +4,6 @@ export CXX=clang++
 export CC=clang
 
 DEPENDENCIES_ROOT="~/dev/openmw-deps"
-#QT_PATH="~/Qt5.5.0/5.5/clang_64/lib/cmake"
 FFMPEG_HOME="/opt/local"
 
 mkdir build
@@ -14,10 +13,8 @@ cmake \
 -D CMAKE_PREFIX_PATH="$DEPENDENCIES_ROOT;$QT_PATH" \
 -D CMAKE_OSX_DEPLOYMENT_TARGET="10.9" \
 -D CMAKE_OSX_SYSROOT="macosx10.11" \
--D CMAKE_BUILD_TYPE=RelWithDebInfo \
--D OPENMW_OSX_DEPLOYMENT=TRUE \
 -D DESIRED_QT_VERSION=4.8 \
 -D BUILD_ESMTOOL=FALSE \
 -D BUILD_MYGUI_PLUGIN=FALSE \
--G"Unix Makefiles" \
+-G"Xcode" \
 ..
