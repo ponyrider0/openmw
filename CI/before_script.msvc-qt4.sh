4457dd792a8a830fc17ea052bb34be22473f3bf7
danielbui78 <danielbui@alumni.ufl.edu>
2018-02-26 18:37:44 -0500

    build scripts

--- CI/before_script.msvc-qt4.sh
+++ CI/before_script.msvc-qt4.sh
@@ -648,7 +648,7 @@ echo "Setting up OpenMW build..."
 add_cmake_opts -DBUILD_BSATOOL=no \
 	-DBUILD_ESMTOOL=no \
 	-DBUILD_MYGUI_PLUGIN=no \
-	-DOPENMW_MP_BUILD=on 
+	-DOPENMW_MP_BUILD=on
 
 if [ ! -z $CI ]; then
 	case $STEP in
